a25ba1418e26669aa59158b04e84b52990e4c36e
EduardAl <eduard_alfons@hotmail.com>
2019-03-23 15:23:59 -0600

    Estructura principal Se crearon los paquetes principales para el proyecto Se creó carpeta .lib para las librerías

--- ProyectoJava/src/sv/com/tesa/ticket/controllers/LoginController.java
+++ ProyectoJava/src/sv/com/tesa/ticket/controllers/LoginController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sv.com.tesa.ticket.controller;
+package sv.com.tesa.ticket.controllers;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
