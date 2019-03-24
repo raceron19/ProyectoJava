650e9b14d84e338e1a08f02c52793881b151ab7a
EduardAl <eduard_alfons@hotmail.com>
2019-03-24 16:44:27 -0600

    Modificación de la conexión. Uso de archivo para guardar datos. Adaptación de bean y conrolador para el Login. Funcional

--- ProyectoJava/src/sv/com/tesa/ticket/models/LoginModel.java
+++ ProyectoJava/src/sv/com/tesa/ticket/models/LoginModel.java
@@ -5,10 +5,43 @@
  */
 package sv.com.tesa.ticket.models;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import sv.com.tesa.ticket.beans.LoginBean;
+
 /**
  *
  * @author vaselinux
  */
-public class LoginModel {
-    
+public class LoginModel extends Conexion
+{
+    public LoginBean validar(String user, String password)
+    {
+        try {
+            LoginBean usuario = new LoginBean();
+            String sql = "CALL sp_select_user(?, ?)";
+            this.conectar();
+            st = conexion.prepareStatement(sql);
+            st.setString(1, user);
+            st.setString(2, password);
+            rs = st.executeQuery();
+            while(rs.next())
+            {
+                System.out.println(rs.getString("Correo"));
+            }  
+            return usuario;
+        } catch (SQLException ex) {
+            Logger.getLogger(LoginModel.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        finally
+        {   
+            try {
+                this.desconectar();
+            } catch (SQLException ex) {
+                Logger.getLogger(LoginModel.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
 }
